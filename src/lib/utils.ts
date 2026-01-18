7a77cedd87dff30e0e5207bb19502e92fa13e12b
lightin <breval2000@live.fr>
2026-01-18 16:14:01 +0100

    Refactor code structure for improved readability and maintainability

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs))
+}
+
+const { version } = require('package.json')
+export default version
